bcfca9bc6793cecd8d54a0d4e1d0d5cec74e20f8
Felihle <ncongwanefelihle02@gmail.com>
2024-01-06 23:14:21 +0000

    Name Convention

---
+++ naming_convention.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	int file_size; //Snake case
+	int FileSize; //Pascal case
+	int fileSize; //Camel case
+	int iFileSize; //Hungarian Notation
+
+	std::cout <<"";
+	return 0;
